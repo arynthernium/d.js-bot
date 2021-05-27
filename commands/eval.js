d95e933fe879cba5f3ed4716c9ed8540f19fdf4c
arynthernium <69891576+arynthernium@users.noreply.github.com>
2021-05-26 23:28:39 -0400

    updated eval to require an expression.

--- commands/eval.js
+++ commands/eval.js
@@ -5,30 +5,38 @@ module.exports = {
 	execute(msg, args, client) {
 		if (msg.author.id == process.env.ADMIN) {
 			var result = msg.content.split(" ").slice(1).join(" ")
-			let evaled = eval(result);
-			const embed = {
-				"description": "**[never use eval!](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#never_use_eval!)**",
-				"url": "https://discordapp.com",
-				"color": 1405905,
-				"footer": {
-					"text": `v${process.env.prefix}`
-				},
-				"fields": [
-					{
-						"name": "input",
-						"value": `\`\`\`${result}\`\`\``
+			if (!result == "") {
+				let evaled = eval(result);
+				const embed = {
+					"description": "**[never use eval!](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#never_use_eval!)**",
+					"color": 1405905,
+					"footer": {
+						"text": `v${process.env.prefix}`
 					},
-					{
-						"name": "output",
-						"value": `\`\`\`${evaled}\`\`\``
-					},
-					{
-						"name": "type",
-						"value": `\`\`\`${typeof evaled}\`\`\``
-					}
-				]
+					"fields": [
+						{
+							"name": "input",
+							"value": `\`\`\`${result}\`\`\``
+						},
+						{
+							"name": "output",
+							"value": `\`\`\`${evaled}\`\`\``
+						},
+						{
+							"name": "type",
+							"value": `\`\`\`${typeof evaled}\`\`\``
+						}
+					]
+				};
+				msg.channel.send({ embed });
+			} else {
+				const embed = {
+					"title": "Syntax error",
+					"color": 14682891,
+					"description": `You used the incorrect syntax.\nPlease refer to Help for usage.\n\`${process.env.prefix}help eval\``
+				};
+				msg.channel.send({ embed });
 			};
-			msg.channel.send({ embed });
-		}
+		};
 	},
 };
